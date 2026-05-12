e413fef8c77cfdd868f50910465f4dedc9046e08
Karol Fugiel <karol109234@gmail.com>
2026-05-12 20:15:54 +0200

    add vocabulary json file

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+import json
+import sqlite3
+import re
+import numpy as np
+from nltk.stem import PorterStemmer
 from scipy.sparse import load_npz
+from make_matrix import stem
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def main():
     tfidf = load_npz("tfidf.npz")
+
+    stemmer = PorterStemmer()
+
+    text = input()
     
-    print(tfidf)
+    processed_text = [stem(text, stemmer)]
+    
+    with open("vocab.json", "r", encoding="utf-8") as f:
+        vocab = json.load(f)
+        
+    print(type(vocab))
+    
+    vectorizer = TfidfVectorizer(
+        min_df=5,
+        stop_words="english",
+        dtype=np.float32,
+    )
+    
+    
 
     
 
