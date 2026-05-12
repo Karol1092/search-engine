4ce2685fc3bf35024180fb2a5104e390524d7720
Karol Fugiel <karol109234@gmail.com>
2026-05-12 21:58:57 +0200

    add ready PoC

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
 import joblib
 import sqlite3
-import re
 import numpy as np
 from nltk.stem import PorterStemmer
 from scipy.sparse import load_npz
 from make_matrix import stem
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+K = 10
 
 def main():
     tfidf = load_npz("tfidf.npz")
     vectorizer = joblib.load("tfidf-vectorizer.pkl")
 
     stemmer = PorterStemmer()
-    text = input()
-    processed_text = stem(text, stemmer)
     
-    print(processed_text)
+    while True:
+        text = input("Search: ")
+        processed_text = stem(text, stemmer)
+        
+        q = vectorizer.transform([processed_text])
+        
+        scores = cosine_similarity(q, tfidf)[0]
+        
+        top_indeces = np.argsort(scores)[-K:][::-1]
+        
+        print("\n")
+        
+        for idx in top_indeces:
+            doc_title, doc_url = get_doc_title_url(str(idx + 1))
+            print(doc_title.replace("_", " "))
+            print(doc_url)
+            print(scores[idx], "\n")
+            
+        print("\n")
     
     
-    q = vectorizer.transform([processed_text])
+def get_doc_title_url(doc_id):
+    conn = sqlite3.connect("articles.db")
+    cur = conn.cursor()
     
-    print(q)
+    cur.execute("SELECT title, url FROM articles WHERE id = ?", (doc_id,))
+    row = cur.fetchone()
     
+    conn.close()
+    
+    return row[0], row[1]
 
 if __name__ == "__main__":
     main()
